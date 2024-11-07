61cb70e4a3e64b3070989590f3225a34eae99e2a
Chinmay <chinmaypanghri@gmail.com>
2024-11-07 19:56:05 +0530

    Update stack.cpp
    removed "using namespace std" so as to "include" only the required keywords (cout and endl)

--- stack.cpp
+++ stack.cpp
@@ -1,23 +1,22 @@
 #include<vector>
 #include<iostream>
-using namespace std;
 
 class Stack{
    private:
-    vector<int> stack;
+    std::vector<int> stack;
    public:
-    void push(int x){                  //add an element to the stack
+    void push(int x){                   //add an element to the stack
         stack.push_back(x);
     }
-    void pop(){                     //remove the top most element
+    void pop(){                         //remove the top most element
         if (stack.empty()){
-            cout<<"Stack is already empty!"<<endl;             
+            std::cout<<"Stack is already empty!"<<std::endl;             
         }    
         else stack.pop_back();  
 }
-    int pop(int x){                  //remove an element at index x
+    int pop(int x){                     //remove an element at index x
       if(!(x>=0 && x<stack.size())){
-        cout<<"Invalid Index. Enter a value between 0 and "<<stack.size()-1<<endl;
+        std::cout<<"Invalid Index. Enter a value between 0 and "<<stack.size()-1<<std::endl;
         return -1;
       }
       for(int i=x;i<stack.size()-1;i++){
@@ -25,26 +24,26 @@ class Stack{
       }
       pop();
     }
-    void print(){                    //print the entire stack
+    void print(){                       //print the entire stack
      if(stack.size()>0){
         for(int i=0;i<stack.size()-1;i++){
-            cout<<stack[i]<<" <- ";
+            std::cout<<stack[i]<<" <- ";
         }
-        cout<<stack[stack.size()-1]<<endl;
+        std::cout<<stack[stack.size()-1]<<std::endl;
     }
-     else cout<<"Stack is empty!"<<endl;
+     else std::cout<<"Stack is empty!"<<std::endl;
 }
     void top(){                         //return top element
       if(stack.empty())
-        cout<<"Stack is empty, no top element!"<<endl;
-     else cout<<"Top most element: "<<stack[stack.size()-1]<<endl;
+        std::cout<<"Stack is empty, no top element!"<<std::endl;
+     else std::cout<<"Top most element: "<<stack[stack.size()-1]<<std::endl;
     }
     void bottom(){                      //return bottom element
       if(stack.empty())
-        cout<<"Stack is empty, no bottom element!"<<endl;      
-     else cout<<"Bottom most element: "<<stack[0]<<endl;
+        std::cout<<"Stack is empty, no bottom element!"<<std::endl;      
+     else std::cout<<"Bottom most element: "<<stack[0]<<std::endl;
     }
-    void clear(){
+    void clear(){                       //empties the stack
       for(int i=stack.size()-1; i>-1;i--){
         pop();
       }
@@ -53,12 +52,9 @@ class Stack{
 int main(){
    Stack stack;
    for(int i=1;i<11;i++){
-    stack.push(1);
+    stack.push(i);
    }
-   stack.print(); 
-   stack.pop();
-   stack.print(); 
-   stack.push(10);
-   stack.push(11);
+   stack.print();
+   stack.clear();
    stack.print();
 }
