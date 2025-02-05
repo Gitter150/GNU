297a8cfd2a51956d6e534c70ee05c53f54bf89b9
Chinmay <chinmaypanghri@gmail.com>
2025-02-05 16:33:00 +0530

    Update linkedList.c
    lets gooo, i literally created a miniature linked list codebase in C. U can use it as a header file and include it in your programs locally.

--- linkedList.c
+++ linkedList.c
@@ -10,6 +10,11 @@ typedef struct {
     Node* head;
 } List;
 
+typedef struct {
+    int success;
+    int data;
+} dataResult;
+
 void insertFront(List* lst, int x){
     Node* first = (Node*)malloc(1*sizeof(Node));
     if (first) {
@@ -103,14 +108,77 @@ void removeLast(List* lst){
     free(temp);
 }
 
+size_t listLen(List* lst){
+    size_t len = 0;
+    Node* current = lst -> head;
+    while(current != NULL) {
+        len++;
+        current = current -> next;
+    }
+    return len;
+}
+
+int indexOfNode(List* lst, Node* node) {
+    int index = 0;
+    Node* current = lst -> head;
+    while(current) {
+        if(current == node) return index;
+        index++;
+        current = current -> next;
+    }
+    return -1;
+}
+
+Node* findNode(List* lst, int key) {
+    Node* foundNode = lst -> head;
+    while(foundNode) {
+        if(foundNode -> data == key){
+            return foundNode;
+        }
+        foundNode = foundNode -> next;
+    }
+    return NULL;
+}
+
+Node* nodeAt(List* lst, int index) {
+    int i = 0;
+    Node* current = lst -> head;
+    while(current) {
+        if(i == index) return current;
+        i++;
+        current = current -> next;
+    }
+    return NULL;
+}
+/*
+void reverseList(List* lst) {             
+    if(lst -> head == NULL) {
+        return;
+    }
+    if((lst -> head) -> next == NULL) {    fellow programmer,
+        return;                            please, if this function is marked as a comment,
+    }                                      please, complete it and push the file to my
+                                           main branch. Thank you. 
+} */
+
+dataResult dataAtNode(Node* node) {
+    if(!node) {
+        return (dataResult) { .success = 0 , .data = 0};  //error code
+    }
+    return (dataResult) { .success = 1, .data = node -> data};
+}
+
 int main() {
     List* lst = createList();
+    int index = 0;
     for(int i=1;i<=5;i++) insertLast(lst, i);
     printList(lst);
     removeFront(lst);
     insertFront(lst,1);
     removeLast(lst);
     printList(lst);
+    printf("The length of the list is %zu\n",listLen(lst));
+    printf(dataAtNode(nodeAt(lst, index)).success? "The data at node of index %d is %d\n":"Error! Node doesn't exist\n",index,dataAtNode(nodeAt(lst, index)).data);
     freeList(lst);
     return 0;
 }
