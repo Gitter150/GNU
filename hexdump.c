d71f6ff8464ccb93c393f4c0e269f827ceb84e3b
Gitter150 <chinmaypanghri@gmail.com>
2025-08-28 13:22:30 +0530

    Added ASCII representation for hexdump CLI tool

--- hexdump.c
+++ hexdump.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define SIZE_T_MAX 0xFFFFFFFFU
+
 int main(int argc, char *argv[]) {
     unsigned char buffer[16];
     FILE *fp;
@@ -11,24 +13,29 @@ int main(int argc, char *argv[]) {
     size_t bytes_read = 0;
     size_t bytes_left_to_read;
     size_t bytes_in_each_line = 16;
-    printf("argc = %d, argv = ", argc);
-    for(int i = 0; i < argc; i++) {
-        printf("%s ",argv[i]);
-    }
-    printf("\n");
+    long long no_of_bytes;
+    // printf("argc = %d, argv = ", argc);
+    // for(int i = 0; i < argc; i++) {
+    //     printf("%s ",argv[i]);
+    // }
+    // printf("\n");
     if(argc < 2) {
         char filepath[100];
         printf("Enter the file path (separator: /) here: ");
         scanf("%99s", filepath);
         printf("You entered: %s\n", filepath);
         printf("Enter the number of bytes to read (-1 if all): ");
-        scanf("%zu", &bytes_left_to_read);
-        if(bytes_left_to_read == 0) {
+        scanf("%lld", &no_of_bytes);
+        if(no_of_bytes == 0) {
             printf("Number of bytes should not be zero. Please try again");
             return 1;
         }
-        bytes_left_to_read == -1? bytes_left_to_read = SIZE_T_MAX : 0;
-        
+        if(no_of_bytes < -1) {
+            printf("Number of bytes cannot be less than -1. Please try again");
+            return 1;
+        }
+        if(no_of_bytes == -1) no_of_bytes = SIZE_T_MAX;
+        bytes_left_to_read = (size_t)no_of_bytes;
         fp = fopen(filepath, "rb");
     } else {
         if(argc == 2) {
@@ -36,12 +43,17 @@ int main(int argc, char *argv[]) {
         } else {
             for(int i = 0; i < argc; i++) {
                 if(strcmp(argv[i], "-n") == 0) {
-                    size_t no_of_bytes = strtoul(argv[i+1], NULL, 10);
+                    no_of_bytes = strtoll(argv[i+1], NULL, 10);
                     if(!no_of_bytes) {
                         printf("Number of bytes should not be zero. Please try again");
                         return 1;
                     }
-                    bytes_left_to_read = no_of_bytes;
+                    if(no_of_bytes < -1) {
+                        printf("Number of bytes cannot be less than -1. Please try again");
+                        return 1;
+                    }
+
+                    bytes_left_to_read = (size_t)no_of_bytes;
                 } // -n flag
             }
         }
@@ -52,6 +64,7 @@ int main(int argc, char *argv[]) {
         printf("Either the filepath is wrong or the file doesn't exist. Please try again.");
         return 1;
     }
+
     int any_bytes_read = 0;
     size_t i = 0;
     int remainder_bytes = bytes_left_to_read % 16;
@@ -73,8 +86,18 @@ int main(int argc, char *argv[]) {
             if(j == 3 || j == 7 || j == 11) printf(" ");
         }
 
+        printf(" |");
+
+        for(int j = 0; j < n; j++) {
+            if(buffer[j] >= 32 && buffer[j] <= 126) {
+                printf("%c",(char)buffer[j]);
+            } else {
+                printf("\033[31m.\033[0m");
+            }
+        }
+
         if( (i == max_iters - 2) && (remainder_bytes != 0) ) bytes_in_each_line = remainder_bytes;
-        printf("\n");
+        printf("|\n");
         index += 16;
         i++;
     } 
